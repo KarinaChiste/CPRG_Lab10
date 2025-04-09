95ea570957f31cf4736ed44022fbfcd994064bce
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-04-09 13:44:56 -0600

    Addition Complete

--- Lab10/Program.cs
+++ Lab10/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace Lab10
+﻿using System.Numerics;
+
+namespace Lab10
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Addition");
+            Console.WriteLine("Enter number n: ");
+            string a = Console.ReadLine();
+            int number1 = Convert.ToInt32(a);
+            Console.WriteLine("Enter number m: ");
+            string b = Console.ReadLine();
+            int number2 = Convert.ToInt32(b);
+            Console.WriteLine(Addition(number1,number2));
         }
-    }
+        
+
+        public static int Addition(int a, int b)
+        {
+            if( a == b)
+            {
+                return a;
+            }
+            else
+            {
+                return a+Addition(a+1, b);
+            }
+                
+        }
+
+        public static int Divisions(int a)
+        {
+            return 0;
+        }
+    } 
 }
